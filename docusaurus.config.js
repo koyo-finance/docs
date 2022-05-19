576d646b8d14531de18429fa21f616c22831dc54
p7m <perpetuum7@protonmail.com>
2022-05-19 08:24:46 +0200

    refactor: custom home page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,19 +6,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-	title: 'My Site',
-	tagline: 'Dinosaurs are cool',
-	url: 'https://your-docusaurus-test-site.com',
+	title: 'Kōyō Finance',
+	tagline: 'Documentation and Guides',
+	url: 'https://docs.koyo.finance',
 	baseUrl: '/',
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
 	favicon: 'img/favicon.ico',
-
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: 'facebook', // Usually your GitHub org/user name.
-	projectName: 'docusaurus', // Usually your repo name.
-
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
 	// to replace "en" with "zh-Hans".
@@ -55,9 +49,9 @@ const config = {
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
 			navbar: {
-				title: 'My Site',
+				title: 'Kōyō Finance',
 				logo: {
-					alt: 'My Site Logo',
+					alt: 'Kōyō Finance Logo',
 					src: 'img/logo.svg'
 				},
 				items: [
