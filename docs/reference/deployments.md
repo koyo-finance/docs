0b1b528b70f86fc082858db2a8cc97371a82a78b
p7m <perpetuum7@protonmail.com>
2022-05-21 16:39:16 +0200

    chore: add contract addresses

---
+++ docs/reference/deployments.md
@@ -0,0 +1,11 @@
+---
+id: deployments
+title: Deployment Addresses
+---
+
+# Kōyō Finance Contract Deployments
+
+| Contract        | Chain ID | Address                                                                                                                               | Source Code                                                                                                                     | Based on                                                                                       |
+| --------------- | -------- | ------------------------------------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------------------------- | ---------------------------------------------------------------------------------------------- |
+| StableSwap4Pool | 288      | [`0x9F0a572be1Fcfe96E94C0a730C5F4bc2993fe3F6`](https://blockexplorer.boba.network/address/0x9F0a572be1Fcfe96E94C0a730C5F4bc2993fe3F6) | [StableSwap4Pool.vy](https://github.com/koyo-finance/exchange-contracts/blob/main/contracts/pools/four-pool/StableSwap4Pool.vy) | https://github.com/curvefi/curve-contract/blob/master/contracts/pools/3pool/StableSwap3Pool.vy |
+| CurveTokenV3    | 288      | [`0xDAb3Fc342A242AdD09504bea790f9b026Aa1e709`](https://blockexplorer.boba.network/address/0xDAb3Fc342A242AdD09504bea790f9b026Aa1e709) | [CurveTokenV3.vy](https://github.com/koyo-finance/exchange-contracts/blob/main/contracts/tokens/CurveTokenV3.vy)                | https://github.com/curvefi/curve-contract/blob/master/contracts/tokens/CurveTokenV3.vy         |
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,9 +13,6 @@ const config = {
 	onBrokenLinks: 'throw',
 	onBrokenMarkdownLinks: 'warn',
 	favicon: 'img/favicon.ico',
-	// Even if you don't use internalization, you can use this field to set useful
-	// metadata like html lang. For example, if your site is Chinese, you may want
-	// to replace "en" with "zh-Hans".
 	i18n: {
 		defaultLocale: 'en',
 		locales: ['en']
@@ -27,16 +24,13 @@ const config = {
 			/** @type {import('@docusaurus/preset-classic').Options} */
 			({
 				docs: {
+					routeBasePath: 'protocol/',
 					sidebarPath: require.resolve('./sidebars.js'),
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/'
+					editUrl: 'https://github.com/koyo-finance/docs/'
 				},
 				blog: {
 					showReadingTime: true,
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/'
+					editUrl: 'https://github.com/koyo-finance/docs/'
 				},
 				theme: {
 					customCss: require.resolve('./src/css/custom.css'),
@@ -50,19 +44,13 @@ const config = {
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
 			navbar: {
-				title: 'Kōyō Finance',
+				title: 'Kōyō Finance Docs',
 				logo: {
 					alt: 'Kōyō Finance Logo',
 					src: 'img/logo.svg'
 				},
 				items: [
-					{
-						type: 'doc',
-						docId: 'intro',
-						position: 'left',
-						label: 'Tutorial'
-					},
-					{ to: '/blog', label: 'Blog', position: 'left' },
+					{ to: '/blog', label: 'Blog', position: 'right' },
 					{
 						href: 'https://github.com/facebook/docusaurus',
 						label: 'GitHub',
@@ -71,60 +59,13 @@ const config = {
 				]
 			},
 			footer: {
-				style: 'dark',
-				links: [
-					{
-						title: 'Docs',
-						items: [
-							{
-								label: 'Tutorial',
-								to: '/docs/intro'
-							}
-						]
-					},
-					{
-						title: 'Community',
-						items: [
-							{
-								label: 'Stack Overflow',
-								href: 'https://stackoverflow.com/questions/tagged/docusaurus'
-							},
-							{
-								label: 'Discord',
-								href: 'https://discordapp.com/invite/docusaurus'
-							},
-							{
-								label: 'Twitter',
-								href: 'https://twitter.com/docusaurus'
-							}
-						]
-					},
-					{
-						title: 'More',
-						items: [
-							{
-								label: 'Blog',
-								to: '/blog'
-							},
-							{
-								label: 'GitHub',
-								href: 'https://github.com/facebook/docusaurus'
-							}
-						]
-					}
-				],
-				copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`
+				// style: 'dark',
+				links: []
 			},
 			colorMode: {
 				// "light" | "dark"
 				defaultMode: 'dark',
-
-				// Hides the switch in the navbar
-				// Useful if you want to support a single color mode
 				disableSwitch: false,
-
-				// Should we use the prefers-color-scheme media-query,
-				// using user system preferences, instead of the hardcoded defaultMode
 				respectPrefersColorScheme: true
 			},
 			prism: {
